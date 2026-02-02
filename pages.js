7d8a06432f8c0d535a731dee0b02937ae3629d61
Stfa75 <u2535501@rts-365.dk>
2026-02-02 08:14:42 +0100

    Steffens Opgave final version 2

---
+++ pages.js
@@ -0,0 +1,11 @@
+const side1 = document.querySelector(".primaryNavigation li")
+ /*her griber jeg fat i linkene*/
+
+side1.addEventListener ("click", skiftside)
+side2.addEventListener ("click", skiftside1)
+side3.addEventListener ("click", skiftside2)
+
+function skiftside(){
+ 
+}
+
